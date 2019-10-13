6eeede779896e42a932c2ba2004f100e4fac1adb
Dezső Klimisz <dklimisz@gmail.com>
2019-10-13 12:55:02 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+define('SERVER_ROOT', $_SERVER['DOCUMENT_ROOT'].'/webprog2/');
+define('SITE_ROOT', 'http://localhost/web2/');
+// define('SITE_ROOT', 'http://dklimisz.cew.hu/webprog2/');
+
+require_once(SERVER_ROOT . 'controllers/' . 'router.php');
+
+?>
